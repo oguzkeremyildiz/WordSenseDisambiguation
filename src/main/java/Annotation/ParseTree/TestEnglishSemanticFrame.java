f15932498f1bb21ff2d2046f22677315024dc924
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-12-31 13:18:24 +0300

    Initial commit.

---
+++ src/main/java/Annotation/ParseTree/TestEnglishSemanticFrame.java
@@ -0,0 +1,13 @@
+package Annotation.ParseTree;
+
+import WordNet.WordNet;
+
+public class TestEnglishSemanticFrame {
+
+    public static void main(String[] args){
+        WordNet englishWordNet = new WordNet("english_wordnet_version_31.xml");
+        WordNet turkishWordNet = new WordNet();
+        new TreeEnglishSemanticFrame(englishWordNet, turkishWordNet);
+    }
+
+}
