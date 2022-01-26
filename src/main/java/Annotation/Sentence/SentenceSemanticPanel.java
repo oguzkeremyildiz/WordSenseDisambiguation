c1267ccf86e80e487f56ff6321b36395f98b2246
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-01-26 19:06:40 +0300

    Added abstract method setLineSpace for generalization of drawing panel.

--- src/main/java/Annotation/Sentence/SentenceSemanticPanel.java
+++ src/main/java/Annotation/Sentence/SentenceSemanticPanel.java
@@ -76,6 +76,11 @@ public class SentenceSemanticPanel extends SentenceAnnotatorPanel {
     }
 
     @Override
+    protected void setLineSpace() {
+        lineSpace = 80;
+    }
+
+    @Override
     protected void drawLayer(AnnotatedWord word, Graphics g, int currentLeft, int lineIndex, int wordIndex, int maxSize, ArrayList<Integer> wordSize, ArrayList<Integer> wordTotal) {
         if (word.getSemantic() != null){
             String correct = word.getSemantic();
